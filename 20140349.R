dfefd9938d2df9cf828ea3f95e1aa948922b7c94
Kathhhy <yke721@gmail.com>
2016-06-22 21:11:04 +0900

    metadata_2016_challenge assignment R code
    combining 4 Ted talks(Art,Free, Music, Time)
    and term clustering

---
+++ 20140349.R
@@ -0,0 +1,148 @@
+library(rjson)
+library(RCurl)
+library(tm)
+library(SnowballC)
+
+###Set working dir path
+setwd("c:/ky")
+
+
+
+####
+#filee = dir(pattern = "json")
+#datas <- lapply(filee,fromJSON(filee, method="C"))
+#finaldata = do.call(rbind,datas)
+
+#####
+j_filea <-"./talks_art.json"
+j_filef <-"./talks_free.json"
+j_filem <-"./talks_music.json"
+j_filet <-"./talks_time.json"
+
+###Read a json file to workspace
+talk_art<-fromJSON(file=j_filea, method="C")
+talk_free<-fromJSON(file=j_filef, method="C")
+talk_music<-fromJSON(file=j_filem, method="C")
+talk_time<-fromJSON(file=j_filet, method="C")
+#str(talk_art)
+#str(talk_free)
+#str(talk_music)
+#str(talk_time)
+##saving column names
+talk_names<-names(talk_music$talks[[1]]$talk)
+talk_names
+###Convertobject type
+talk_art_list<-lapply(talk_art$talks, function(x){unlist(x)})
+talk_free_list<-lapply(talk_free$talks, function(x){unlist(x)})
+talk_music_list<-lapply(talk_music$talks, function(x){unlist(x)})
+talk_time_list<-lapply(talk_time$talks, function(x){unlist(x)})
+str(talk_music_list)
+parse_talk_all<-data.frame()
+##combining datasets
+df_parse_talk_all<-do.call("rbind", c(parse_talk_all,talk_art_list,talk_free_list, talk_music_list,talk_time_list))
+
+str(df_parse_talk_all)
+
+df_parse_talk_all<-data.frame(df_parse_talk_all)
+str(df_parse_talk_all)
+###Chage datatype of talk description
+df_parse_talk_all$talk.description<-as.character(df_parse_talk_all$talk.description)
+str(df_parse_talk_all$talk.description)
+
+###Change names of variables
+names(df_parse_talk_all)<-talk_names
+str(df_parse_talk_all)
+
+## removing repetition
+total<-df_parse_talk_all[ !duplicated( df_parse_talk_all$id) , ] 
+
+
+#####Term Clustering
+
+###Convert object type
+class(total$description)
+ted_docs <- Corpus(VectorSource(total$description))
+class(ted_docs)
+
+###Pre-processing
+ted_docs <- tm_map(ted_docs, tolower)
+ted_docs <- tm_map(ted_docs, removeNumbers)
+ted_docs <- tm_map(ted_docs, removePunctuation)
+ted_docs <- tm_map(ted_docs, removeWords, stopwords("SMART"))
+ted_docs <- tm_map(ted_docs, removeWords, "ted")
+
+###Tokenizing
+strsplit_space_tokenizer <- function(x)
+  unlist(strsplit(as.character(x), "[[:space:]]+"))
+#token_docs<-(sapply(ted_docs, strsplit_space_tokenizer))
+token_docs<-(sapply(ted_docs$content, strsplit_space_tokenizer))
+##token_docs<-(sapply(token_docs, function(x){gsub(" "," ",x)}))
+token_freq<-table(unlist(token_docs))
+summary(data.frame(token_freq)$Freq)
+
+###Stemming
+stem_docs <- sapply(token_docs, stemDocument)
+stem_freq<-table(unlist(stem_docs))
+summary(data.frame(stem_freq)$Freq)
+
+total_freq<-data.frame(stem_freq)
+str(total_freq)
+
+
+###Term-Doc Matrix with Stemming
+class(stem_docs)
+stem_docs <- Corpus(VectorSource(stem_docs))
+class(stem_docs)
+
+###term weight: TfIDF
+ted_tdm <- TermDocumentMatrix(stem_docs,
+                              control = list(removePunctuation = TRUE,
+                                             weighting=weightTfIdf,
+                                             stopwords = TRUE))
+
+inspect(ted_tdm[1,])
+
+
+#####Hierachical Clustering: Term Clustering
+###Remove sparse terms
+ted_tdm_sparse <- removeSparseTerms(ted_tdm, sparse = 0.95)
+ted_tdm_sparse$nrow
+ted_tdm<-ted_tdm_sparse
+
+###Convert to matrix
+ted_m <- as.matrix(ted_tdm)
+
+###Calculate similarity
+###dist {stats} Distance Matrix Computation
+###scale {base} Scaling and Centering of Matrix-like Objects
+distMatrix<- dist(scale(ted_m))
+
+###Execute hierarchial clustering
+###hclust {stats} Hierarchical Clustering
+###method=c("single", complete", "average", "mcquitty", "median, "centroid", "ward.D", "ward.D2)
+fit <- hclust(distMatrix, method="average")
+
+###Draw dendrogram
+plot(fit)
+###rect.hclust {stats} Draw Rectangles Around Hierarchical Clusters
+###k = number of clusters
+rect.hclust(fit, k=10)
+
+###Save the dendrogram as PNG image file
+png("./dendrogram_tc_tfidf_average_sparse95.png", width = 1500, height=800)
+plot(fit)
+###k= number of clusters
+rect.hclust(fit, k=10)
+dev.off()
+
+###Assign a cluster to a term
+###cutree {stats} Cut a Tree into Groups of Data
+###k= number of clusters
+groups <- cutree(fit, k=10)
+df_groups <- data.frame(groups)
+str(df_groups)
+df_groups$KWD <- rownames(df_groups)
+str(df_groups)
+
+###Write the clustering result to text file
+write.table(df_groups, "./tc_tfidf_average_0.95.txt", row.names=FALSE, col.names=TRUE, sep="\t")
